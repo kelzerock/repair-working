91e52f236273b8771e66d475ca76537a42b29dad
Ihar Pavetka <uropb.86@gmail.com>
2023-04-10 00:14:56 +0300

    Revert "Main page"

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,12 @@
-import Header from '../Header/Header.component';
+import { AppProps } from "next/app";
+import Header from "../header/Header.component";
 import { Footer } from '../footer';
-import style from './layout.module.scss'
 
 const Layout = ({ children }:any) => {
   return (
     <>
       <Header />
-      <div className={style.layout_main}>
-        {children}
-      </div>
+      <div>{children}</div>
       <Footer />
     </>
   );
