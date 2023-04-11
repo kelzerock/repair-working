fcf2d8f25819f69aab49ca365c47911e55c35f09
Igor Pavetka <uropb.86@gmail.com>
2023-04-11 10:56:26 +0300

    feat: add adative and response for benefits

--- src/components/benefits/benefits.tsx
+++ src/components/benefits/benefits.tsx
@@ -1,26 +1,10 @@
 import classNames from 'classnames';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import style from './benefits.module.scss';
 import iconSpoiler from '../../../public/images/services/icon_spoiler_blue.svg';
 import textBenefits from '../../constants/json/benefits.json';
 
-
-function useDelayUnmount(isMounted: boolean, delayTime: number) {
-    const [showDiv, setShowDiv] = useState(false);
-    useEffect(() => {
-      let timeoutId : string | number | NodeJS.Timeout | undefined;
-      if (isMounted && !showDiv) {
-        setShowDiv(true);
-      } else if (!isMounted && showDiv) {
-        timeoutId = setTimeout(() => setShowDiv(false), delayTime); //delay our unmount
-      }
-      return () => clearTimeout(timeoutId); // cleanup mechanism for effects , the use of setTimeout generate a sideEffect
-    }, [isMounted, delayTime, showDiv]);
-    return showDiv;
-  }
-
-  
 export const Benefits = () => {
     const [isShowing, setIsShowing] = useState(false);
 
@@ -39,7 +23,7 @@ export const Benefits = () => {
             >
                 {textBenefits.map((el, ind) => (
                     <li key={`benefits-${ind}`} className={style.list_li}>
-                        <Image src={el.image} alt={el.title} width={74} height={70}/>
+                        <Image src={el.image} alt={el.title} width={74} height={70} className={style.img} />
                         <h4 className={style.list_title}>{el.title}</h4>
                         <p className={style.list_desc}>{el.desc}</p>
                     </li>
