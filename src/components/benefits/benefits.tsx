eea8e3632334005d82bf2aa24b06086d5fffb4b5
Igor Pavetka <uropb.86@gmail.com>
2023-04-11 09:04:28 +0300

    feat: add spoiler for benefits

--- src/components/benefits/benefits.tsx
+++ src/components/benefits/benefits.tsx
@@ -1,18 +1,42 @@
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import style from './benefits.module.scss';
 import iconSpoiler from '../../../public/images/services/icon_spoiler_blue.svg';
 import textBenefits from '../../constants/json/benefits.json';
 
+
+function useDelayUnmount(isMounted: boolean, delayTime: number) {
+    const [showDiv, setShowDiv] = useState(false);
+    useEffect(() => {
+      let timeoutId : string | number | NodeJS.Timeout | undefined;
+      if (isMounted && !showDiv) {
+        setShowDiv(true);
+      } else if (!isMounted && showDiv) {
+        timeoutId = setTimeout(() => setShowDiv(false), delayTime); //delay our unmount
+      }
+      return () => clearTimeout(timeoutId); // cleanup mechanism for effects , the use of setTimeout generate a sideEffect
+    }, [isMounted, delayTime, showDiv]);
+    return showDiv;
+  }
+
+  
 export const Benefits = () => {
     const [isShowing, setIsShowing] = useState(false);
 
     return (
         <div className={classNames(style.section, 'container')}>
-            <h2 className={style.title}>Преимущества для Вас</h2>
-            <span><Image src={iconSpoiler} alt='spoiler'/></span>
-            <ul className={style.list_ul}>
+            <div
+                className={style.list_header}
+                onClick={() => setIsShowing(!isShowing)}
+                role='presentation'
+            >
+                <h2 className={style.title}>Преимущества для Вас</h2>
+                <span><Image src={iconSpoiler} alt='spoiler' className={classNames({'rotate': !isShowing})}/></span>
+            </div>
+            <ul
+                className={classNames(style.list_ul, {[style.list_hidden] : isShowing})}
+            >
                 {textBenefits.map((el, ind) => (
                     <li key={`benefits-${ind}`} className={style.list_li}>
                         <Image src={el.image} alt={el.title} width={74} height={70}/>
