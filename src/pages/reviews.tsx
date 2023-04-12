84883d6bb688fdcfa701b33b12c4c3f3d6b752a7
Igor Pavetka <uropb.86@gmail.com>
2023-04-12 15:59:55 +0300

    feat: add review-page and refactor reviews-component

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,5 +1,60 @@
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import style from './reviews.module.scss';
+import textReview from '../constants/json/reviews.json';
+import iconArrow from '../../public/images/reviews/icon_like.svg';
+import iconCrumbs from '../../public/images/reviews/icon_crumbs.svg';
+import iconSpoiler from '../../public/images/services/icon_spoiler_blue.svg';
+import classNames from 'classnames';
+import { ReviewCardLand } from '@/components/reviews/review-card-land';
+
 const ReviewPage = () => {
-  return <div>Information about repair.</div>;
-};
+	const [isShowing, setIsShowing] = useState(false);
+
+
+	return (
+		<div className={classNames(style.section, 'container')}>
+			<div className={style.crumbs_wrapper}>
+				<Link href={'/'}>Главная</Link>
+				<Image src={iconCrumbs} alt='crumbs' className={style.crumbs_icon}/>
+				<p>Отзывы</p>
+			</div>
+			<h1 className={style.section_title}>отзывы</h1>
+			<div className={style.head_wrapper}>
+				<div className={style.head_desc}>
+					<p className={style.head_desc_text}>{textReview.total.desc}</p>
+					<p className={style.head_desc_text_add}>Если вы не удовлетворены работой мастера, напишите претензию на <a href={`mailto:${textReview.total.email}`}>{textReview.total.email}</a>. В письме укажите адрес проведённого ремонта для идентификации заявки.</p>
+				</div>
+                <div className={style.score_wrapper}>
+                    <div className={style.score_div}>
+                        <Image src={iconArrow} alt='arrow to right'/>
+                        <p className={style.score_num}>{textReview.total.percent}%</p>
+                        <p className={style.score_text}>положительных отзывов</p>
+                    </div>
+                    <div className={style.score_div_desc}>
+                        <p className={style.score_desc}>{textReview.total.count} отзыва оставили клиенты</p>
+                    </div>
+                </div>
+			</div>
+			<button className={style.btn}>Оставить отзыв</button>
+			<div
+                className={style.list_header}
+                onClick={() => setIsShowing(!isShowing)}
+                role='presentation'
+            >
+                <h3 className={style.title}>Показать все отзывы</h3>
+                <span><Image src={iconSpoiler} alt='spoiler' className={classNames({'rotate': !isShowing})}/></span>
+            </div>
+            <ul
+                className={classNames(style.list_ul, {[style.list_hidden] : isShowing})}
+            >
+				{textReview.reviews.map((el, ind) => 
+					<ReviewCardLand key={`card-review-${ind}`} {...el}/>
+				)}
+            </ul>
+		</div>
+	)
+}
 
 export default ReviewPage;
