2cc55f129cdeb0989ebe78d0b58b8d5d319c0e1e
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-13 00:37:26 +0300

    feat: complete price page

--- src/pages/price.tsx
+++ src/pages/price.tsx
@@ -1,27 +1,50 @@
 import style from "./price.module.scss";
-import textServices from "../constants/json/services.json";
+import textServices from "@/constants/json/services.json";
 import Image from "next/image";
-import RepairInfo from "@/components/Repair-block/Repair-info/Repair-info.component";
 import { useState } from "react";
+import priceListInfo from "@/constants/json/repaire-price-2.json";
+import classNames from "classnames";
+
+const x: any = priceListInfo;
 
 const PricePage = () => {
-  const [isShowListPrice, setIsShowListPrice] = useState(false)
-  const [filter, setFilter] = useState('')
+  const [isShowListPrice, setIsShowListPrice] = useState(false);
+  const [filter, setFilter] = useState("");
+
+  const handleClick = (info: string) => {
+    setIsShowListPrice(!isShowListPrice);
+    setFilter(info);
+  };
 
   return (
     <div className={style.container_price}>
       <h2>СТОИМОСТЬ УСЛУГ ПО РЕМОНТУ</h2>
       <div className={style.section__services}>
         {textServices.services[0].list.map((el, ind) => (
-          <button key={`button-list-${ind}`} className={style.section__btn}>
+          <div
+            key={`button-list-${ind}`}
+            className={classNames(style.section__btn, el.filterName === filter && filter ? style.action : "")}
+            onClick={() => handleClick(el.filterName)}
+          >
             <Image src={el.image} alt={el.name} width={32} height={32} />
             <span>{el.name}</span>
-          </button>
+          </div>
         ))}
       </div>
 
       <div className={style.price_list}>
-        
+        <ul className={style.list_ul}>
+          {filter
+            ? x[filter].map((el: any, ind: any) => (
+                <li key={`price-${ind}`} className={style.list_li}>
+                  <h4 className={style.main_info}>{el.description}</h4>
+                  <h4 className={style.main_price}>от {el.price} руб.</h4>
+                  <p className={style.main_time}>{el.time}</p>
+                  <p className={style.main_guaranty}>{el.guaranty}</p>
+                </li>
+              ))
+            : ""}
+        </ul>
       </div>
     </div>
   );
