35bef3976a08fa8d132e19c0c10cff4fdf0c11d3
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-15 21:40:50 +0300

    fix: fix firm information in header

--- src/components/Header/Logo/logo.component.tsx
+++ src/components/Header/Logo/logo.component.tsx
@@ -1,15 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import logoImg from "../../../../public/wrench.svg";
+import logoImg from "public/images/logo.png";
 import style from "./logo.module.scss";
 
 const Logo = () => {
   return (
     <div className={style.logo}>
       <Link href="/">
-        <h2>РемБыт</h2>
-        <p>Ремонт и установка бытовой техники</p>
-        <Image src={logoImg} alt="logo" width={30} height={30} />
+        <Image src={logoImg} alt="logo" width="0" height="0" sizes="100vw" />
       </Link>
     </div>
   );
