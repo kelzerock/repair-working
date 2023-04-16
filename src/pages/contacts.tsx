35863b9dc636cbc4f25979293af0248088bd2e13
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-16 19:44:30 +0300

    fix: fix some mistakes

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,5 +1,33 @@
+import style from "./contacts.module.scss";
+import infoAboutFirm from "@/constants/json/about-firm.json";
+import Link from "next/link";
+
 const ContactPage = () => {
-  return <div>Information about contact.</div>;
+  return (
+    <div className={style.block}>
+      <h2>НАШИ КОНТАКТЫ</h2>
+      <p>
+        Мы выполним ремонт в удобное для вас время. Мастера нашей компании
+        выезжают по всей территории г.Минска и Минского района.
+      </p>
+      <p>Обращайтесь по нашим телефонам:</p>
+      <ul>
+        <li>{infoAboutFirm.phone[0]}</li>
+        <li>{infoAboutFirm.phone[1]}</li>
+        <li>{infoAboutFirm.phone[2]}</li>
+      </ul>
+      <p>
+        Прием заявок на ремонт/установку принимается с{" "}
+        {infoAboutFirm.time.start} до {infoAboutFirm.time.end} ежедневно и без
+        выходных!
+      </p>
+      <p>А так же вы можете оставить On-Line заявку на нашем сайте.</p>
+      <p>
+        E-mail:
+        <a href={`mailto: ${infoAboutFirm.email}`}> {infoAboutFirm.email}</a>
+      </p>
+    </div>
+  );
 };
 
 export default ContactPage;
