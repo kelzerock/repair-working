f26bd73639368115d9f6f367ab00e0532b80c3d2
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-17 00:57:57 +0300

    fix: add new logo img

--- src/components/Header/Logo/logo.component.tsx
+++ src/components/Header/Logo/logo.component.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
-import logoImg from "/images/logo.png";
 import style from "./logo.module.scss";
 
 const Logo = () => {
   return (
     <div className={style.logo}>
       <Link href="/">
-        <Image src="/images/logo_item.png" alt="logo" width="0" height="0" sizes="100vw" />
+        <Image src="/images/logo_new.png" alt="logo" width="0" height="0" sizes="100vw" />
       </Link>
     </div>
   );
