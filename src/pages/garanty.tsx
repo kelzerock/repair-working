9f3d779bdbd71177b6f08838053b8c273155c17e
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-16 21:19:16 +0300

    fix: some bags

---
+++ src/pages/garanty.tsx
@@ -0,0 +1,12 @@
+import GarantyInfo from "@/components/garanty/GarantyInfo";
+import style from "./garanty.module.scss";
+
+const Garanty = () => {
+  return (
+    <div className={style.block}>
+      <GarantyInfo />
+    </div>
+  );
+};
+
+export default Garanty;
