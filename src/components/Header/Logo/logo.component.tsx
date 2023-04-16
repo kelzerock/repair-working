19f80d9762f4ad1f66e9e3eb66919014dd111c3a
Aleksei Zhuchkov <kelzzzer@gmail.com>
2023-04-16 14:29:34 +0300

    fix: fix logo

--- src/components/Header/Logo/logo.component.tsx
+++ src/components/Header/Logo/logo.component.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
-import logoImg from "public/images/logo.png";
+import logoImg from "/images/logo.png";
 import style from "./logo.module.scss";
 
 const Logo = () => {
   return (
     <div className={style.logo}>
       <Link href="/">
-        <Image src={logoImg} alt="logo" width="0" height="0" sizes="100vw" />
+        <Image src="/images/logo_item.png" alt="logo" width="0" height="0" sizes="100vw" />
       </Link>
     </div>
   );
