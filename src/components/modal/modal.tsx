79eb764132387193fe24882c0f8ce45670013c60
Igor Pavetka <uropb.86@gmail.com>
2023-04-22 02:28:04 +0300

    feat: add use-form-validation for modal window, add loader for fetch, add error message on fetch

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -5,13 +5,24 @@ import {
 	useRef,
 	useState,
 } from 'react';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import classNames from 'classnames';
 
 import closeIcon from '../../../public/images/modal/Icon_close.svg';
+import loaderIcon from '../../../public/images/modal/Loader.svg';
 
 import style from './modal.module.scss';
 import Image from 'next/image';
+import MaskedInput from 'react-text-mask';
 import { useScrollLock } from '@/hooks/scroll-lock';
+import { PhoneMasks } from '@/constant/regex';
+
+export type FormType = {
+	name: string | undefined,
+	address: string | undefined,
+	phone: string,
+	message: string,
+}
 
 export type ModalPropsType = {
 	isShowingModal: boolean;
@@ -80,57 +91,27 @@ export const Modal = ({
 	typeOfModal,
 }: ModalPropsType) => {
 	const modalRef = useRef<HTMLDivElement>(null);
+	const { register, handleSubmit, control, formState: {errors, isValid} } = useForm<FormType>({ mode: 'all' });
 	const { isReview, isRequest, isWarranty, isCall } =
-		useTypeOfModal(typeOfModal);
-
-	const [modalName, setModalName] = useState('');
-	const [modalPhone, setModalPhone] = useState('');
-	const [modalAddress, setModalAddress] = useState('');
-	const [modalText, setModalText] = useState('');
+	useTypeOfModal(typeOfModal);
+	
 	const [isSuccess, setIsSuccess] = useState(false);
+	const [isError, setIsError] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 	const { unlockScroll } = useScrollLock();
-	const [validation, setValidation] = useState(false);
-
-	const validationHandler = () => {
-		if (typeOfModal === 'call') {
-			modalName.trim() && modalPhone.length === 11
-				? setValidation(true)
-				: setValidation(false);
-		}
-		if (typeOfModal === 'review') {
-			modalName.trim() && modalText.trim()
-				? setValidation(true)
-				: setValidation(false);
-		}
-		if (typeOfModal === 'request' || typeOfModal === 'warranty') {
-			modalName.trim() &&
-			modalPhone.length === 11 &&
-			modalAddress.trim() &&
-			modalText.trim()
-				? setValidation(true)
-				: setValidation(false);
-		}
-	};
-
-	useEffect(() => {
-		validationHandler();
-	}, [modalName, modalAddress, modalPhone, modalText]);
-
-	const submitHandler = async () => {
+	
+	const onSubmit: SubmitHandler<FormType> = async (data) => {
+		setIsLoading(true);
 		const requestOptions = {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ TITLE: modalText }),
+			body: JSON.stringify({ TITLE: data.message }),
 		};
-
 		fetch(
 			`https://b24-jk9jbm.bitrix24.by/rest/1/pv44ctw5ojrpgsrl/crm.lead.add.json?`,
 			requestOptions
-		).then(() => setIsSuccess(true));
-		// setIsShowingModal(!isShowingModal);
-		// unlockScroll();
+		).then(() => setIsSuccess(true)).catch(() => setIsError(true));
 	};
-
 	const closeHandler = useCallback(() => {
 		setIsShowingModal(false);
 		unlockScroll();
@@ -142,7 +123,6 @@ export const Modal = ({
 				!modalRef.current?.contains(e.target as Node) &&
 				isShowingModal
 			) {
-				// setIsShowingModal(false);
 				closeHandler();
 			}
 		};
@@ -168,8 +148,16 @@ export const Modal = ({
                         <p className={style.gratitude}>{isReview ? 'Нам важно Ваше мнение.' : 'Мы рассмотрим Ваше обращение в самые кратчайшие сроки.'}</p>
                     </div>
                 }
-                {!isSuccess &&
+                {isError &&
                     <div className={style.modal}>
+                        <h3 className={style.title}>Что-то пошло не так. Попробуйте позже</h3>
+                    </div>
+                }
+                {!isSuccess && !isError &&
+                    <form className={style.modal}
+					onSubmit={handleSubmit(onSubmit)}
+					>
+						
                         {isReview && (
                             <h3 className={style.title}>Оставить отзыв</h3>
                         )}
@@ -188,77 +176,116 @@ export const Modal = ({
                             <input
                                 className={classNames(
                                     style.input,
-                                    !modalName.trim() ? style.invalid : ''
+									{[style.invalid]: errors.name}
                                 )}
                                 placeholder='Фамилия и имя'
-                                onChange={(e) => setModalName(e.target.value)}
-                                required
+								{...register('name', {
+									required: {value: true, message: 'Поле не может быть пустым'},
+									pattern: {value: /^[А-Яа-я]+$/i, message: 'Используйте кириллицу'}
+								})}
                             />
                             <span className={style.placeholder}>Фамилия и имя</span>
+							{errors.name &&
+								<span className={style.error}>
+									{errors.name.message}
+								</span>
+							}
                         </label>
                         {(isRequest || isWarranty) && (
                             <label className={style.label}>
                                 <input
                                     className={classNames(
                                         style.input,
-                                        !modalAddress.trim() ? style.invalid : ''
+										{[style.invalid]: errors.address}
                                     )}
                                     placeholder='Адрес'
-                                    onChange={(e) =>
-                                        setModalAddress(e.target.value)
-                                    }
-                                    required
-                                />
+									{...register('address', {
+										required: {value: true, message: 'Поле не может быть пустым'},
+										pattern: {value: /^[А-Яа-я]+$/i, message: 'Используйте кириллицу'}
+									})}
+								/>
                                 <span className={style.placeholder}>Адрес</span>
+								{errors.address &&
+	                                <span className={style.error}>
+										{errors.address.message}
+									</span>
+								}
                             </label>
                         )}
-                        {(isRequest || isWarranty || isCall) && (
-                            <label className={style.label}>
-                                <input
-                                    type='number'
-                                    className={classNames(
-                                        style.input,
-                                        !(modalPhone.length === 11)
-                                            ? style.invalid
-                                            : ''
-                                    )}
-                                    placeholder='Номер телефона'
-                                    onChange={(e) => setModalPhone(e.target.value)}
-                                    required
-                                />
-                                <span className={style.placeholder}>
-                                    Номер телефона (11 цифр)
-                                </span>
-                            </label>
-                        )}
+                        {(isRequest || isWarranty || isCall) &&
+							<Controller
+								control={control}
+								name="phone"
+								rules={{
+									required: 'Поле не может быть пустым',
+									validate: {
+										hasXsymbols: (value) => value.match(PhoneMasks.placeholderChar) === null || 'В формате +375 (xx) xxx-xx-xx',
+										hasWrongCode: (value) => value.match(PhoneMasks.codePhoneRegex) !== null || 'Проверьте код оператора'}
+									}}
+								render={({
+									field,
+								}) => (
+									<label className={style.label}>
+										<MaskedInput
+											name="phone"
+											value={field.value}
+											type='tel'
+											inputMode='tel'
+											mask={PhoneMasks.phoneMask}
+											placeholderChar='x'
+											className={classNames(style.input,
+												{[style.invalid]: errors.phone}
+											)}
+											placeholder='Номер телефона'
+											guide={true}
+											onChange={field.onChange}
+											onBlur={field.onBlur}
+										/>
+										<span className={style.placeholder}>Номер телефона</span>
+										{errors.phone &&
+											<span className={style.error}>
+												{errors.phone.message}
+											</span>
+										}
+									</label>
+								)}
+							/>
+                        }
                         {!isCall && (
-                            <textarea
-                                className={classNames(
-                                    style.textarea,
-                                    !modalText.trim() ? style.invalid : ''
-                                )}
-                                placeholder={
-                                    isReview
-                                        ? 'Опишите Ваше впечатление о проделанной нами работе.'
-                                        : isRequest
-                                        ? 'Опишите возникшую проблему (Вид и модель техники, неисправность)'
-                                        : isWarranty
-                                        ? 'Опишите Ваш гарантийный случай, с указанием сведений из гарантийного талона'
-                                        : ''
-                                }
-                                required
-                                defaultValue={modalText}
-                                onChange={(e) => setModalText(e.target.value)}
-                            />
+							<label className={style.label_area}>
+								<textarea
+									className={classNames(
+										style.textarea,
+										{[style.invalid]: errors.message}
+									)}
+									placeholder={
+										isReview
+											? 'Опишите Ваше впечатление о проделанной нами работе.'
+											: isRequest
+											? 'Опишите возникшую проблему (Вид и модель техники, неисправность)'
+											: isWarranty
+											? 'Опишите Ваш гарантийный случай, с указанием сведений из гарантийного талона'
+											: ''
+									}
+									{...register('message', {
+										required: {value: true, message: 'Поле не может быть пустым'}
+									})}
+								/>
+								{errors.message &&
+									<span className={style.error}>
+										{errors.message.message}
+									</span>
+								}
+							</label>
                         )}
                         <button
                             className={classNames(
                                 style.btn,
-                                !validation ? style.form_invalid : ''
                             )}
-                            onClick={submitHandler}
+							disabled={isLoading || !isValid}
                             type='submit'
                         >
+							{isLoading && <Image className={style.loader} src={loaderIcon} width={15} height={15} alt='loading'/>}
                             {isReview
                                 ? 'Оставить отзыв'
                                 : isRequest
@@ -269,7 +296,7 @@ export const Modal = ({
                                 ? 'Мы перезвоним Вам'
                                 : ''}
                         </button>
-                    </div>
+                    </form>
                 }
 			</div>
 		</div>
